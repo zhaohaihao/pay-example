55214cabdb60a8eed211c327e3a3254cd4e74a7c
zhaohaihao <zhh2933@gmail.com>
2019-11-21 15:41:58 +0800

    初始提交

---
+++ src/main/java/com/zhh/pay/payexample/PayExampleApplication.java
@@ -0,0 +1,13 @@
+package com.zhh.pay.payexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhh/pay/payexample/PayExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhh.pay.payexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
