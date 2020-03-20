6861f3317eeac0a598fadf5397943788ff5124fc
zhaohaihao <zhh2933@gmail.com>
2020-03-20 18:15:49 +0800

    :pencil: 修改sql文件

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -51,7 +51,7 @@ DROP TABLE IF EXISTS `mall_pay_info`;
 CREATE TABLE `mall_pay_info` (
   `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '支付信息ID',
   `user_id` INT(11) DEFAULT NULL COMMENT '用户ID',
-  `order_no` BIGINT(20) DEFAULT NULL COMMENT '订单号',
+  `order_no` BIGINT(20) NOT NULL COMMENT '订单号',
   `pay_platform` INT(10) DEFAULT NULL COMMENT '支付平台: 1-支付宝, 2-微信',
   `platform_number` VARCHAR(200) DEFAULT NULL COMMENT '支付平台支付流水号',
   `platform_status` VARCHAR(20) DEFAULT NULL COMMENT '支付平台支付状态',
